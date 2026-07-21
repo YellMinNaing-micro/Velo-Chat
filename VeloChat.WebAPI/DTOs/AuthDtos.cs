19cfdc97b80ad9f3bddfbd66bfa64d02bdba6110
Yell Min Naing <yellminnaing5@gmail.com>
2026-07-21 11:45:50 +0630

    Update AuthDtos.cs

--- VeloChat.WebAPI/DTOs/AuthDtos.cs
+++ VeloChat.WebAPI/DTOs/AuthDtos.cs
@@ -3,3 +3,6 @@ namespace VeloChat.WebAPI.DTOs;
 public record RegisterDto(string Username, string Email, string Password, string? ProfilePictureUrl, string? FullName);
 public record LoginDto(string Email, string Password);
 public record TokenDto(string AccessToken, string RefreshToken);
+public record UserProfileDto(string Id, string Username, string Email, string? FullName, string? ProfilePictureUrl);
+public record UpdateProfileDto(string Username, string Email, string? FullName, string? ProfilePictureUrl);
+public record ChangePasswordDto(string OldPassword, string NewPassword);
