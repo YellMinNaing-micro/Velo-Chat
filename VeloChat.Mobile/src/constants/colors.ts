7a2ffd96096a3edcee9f19ffa1a9d606821a200d
Yell Min Naing <yellminnaing5@gmail.com>
2026-07-21 13:42:55 +0630

    Create colors.ts

---
+++ VeloChat.Mobile/src/constants/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  background: '#F7F8F8',
+  surface: '#FFFFFF',
+  surfaceMuted: '#F0F2F1',
+  text: '#17201F',
+  textMuted: '#78827F',
+  border: '#E7EAE9',
+  primary: '#0BA99B',
+  primaryDark: '#087D75',
+  primarySoft: '#E4F8F5',
+  danger: '#D94A56',
+  warning: '#EE792D',
+  success: '#22A46F',
+} as const;
